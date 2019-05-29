411171869611f8c39fae9e375d6c929b3107c6bb
noc1992 <noc817@gmail.com>
2019-05-29 17:53:15 +0900

    commit

---
+++ Part3/sec1-3.R
@@ -0,0 +1,11 @@
+getwd()
+print('안녕하세요 세계인')
+print(pi, digits=3)
+print(3,4)
+
+#indentation 조건식 반복문 함수 등의 긴 문장을 시작할때는 들여쓰기 오와 열 
+#comment 다시 읽거나 다른사람이 읽을때를 위해 주석을 달아서 읽고 이해하기 편하게 해야함 친절함 
+
+print('a', 'b')
+cat('a', 'b')
+3;4
---
+++ Part3/sec4_factors.R
@@ -0,0 +1,50 @@
+print(15%/%2)        # 나눗셈의 몫
+print(5%%2)           # 나눗셈의 나머지 
+print(5^2)
+
+10000000 + 10000000
+1/10000000000
+
+as.numeric('1') + as.numeric('2')
+
+'first'
+"second"
+first 
+first <- 1
+first
+second
+
+3 & 1
+3 | 0
+!0
+!3
+
+cat(1, NA, 2)
+cat(1, NULL, 2)
+
+sum(1, NA, 2)
+sum(1, NULL, 2)
+
+sum(1, NA, 2, na.rm = T)    #결측치 처리법
+
+
+txt1 <- read.csv("factor_test.csv")
+txt1
+
+factor1 <- factor(txt1$blood)
+factor1
+factor2 <- factor(txt1$location)
+factor2
+summary(factor1, stringasFactors = FALSE)
+View(txt1)
+
+
+
+
+
+
+
+
+
+
+
