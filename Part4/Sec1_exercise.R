a75c568157462715cd37364fc6fe1df41afba553
noc1992 <noc817@gmail.com>
2019-06-08 14:20:10 +0900

    Part4 exercise end

--- Part4/Sec1_exercise.R
+++ Part4/Sec1_exercise.R
@@ -1,5 +1,7 @@
 library(dplyr)
-iris
+library("RColorBrewer")
+library(wesanderson)
+View(iris)
 #########1번 입니다.##############
 data1 <- filter(iris, Species =="setosa")
 x1 <- select(data1, Sepal.Length, Sepal.Width)
@@ -27,40 +29,37 @@ plot(z2)
 
 ########################2번입니다##################
 # ---(1) 세로 --- #
-seto_b1 <- data1 %>%
+all_b1 <- iris %>%
   group_by(Species) %>%
   summarise_each(funs(mean), Sepal.Length,Sepal.Width,Petal.Length,Petal.Width)
-seto_b1
-Set <- data.frame(seto_b1)
-row.names(Set) <- Set$Species
-Set <- subset(Set, select = c(Sepal.Length,Sepal.Width,Petal.Length,Petal.Width))
-Set
+all_b1 <- subset(all_b1, select = c(Sepal.Length,Sepal.Width,Petal.Length,Petal.Width))
+all_b1
 
-ver_b1 <- data3 %>%
-  group_by(Species) %>%
-  summarise_each(funs(mean), Sepal.Length,Sepal.Width,Petal.Length,Petal.Width)
-Ver <- data.frame(ver_b1)
-row.names(Ver) <- Ver$Species
-Ver <- subset(Ver, select = c(Sepal.Length,Sepal.Width,Petal.Length,Petal.Width))
-Ver
+barp <- data.frame(all_b1)
 
+Tr <- t(barp)
+label <- c("Setosa", "Versicolor","virginica")
+colnames(Tr) <- label
+Tr
 
-vir_b1 <- data5 %>%
-  group_by(Species) %>%
-  summarise_each(funs(mean), Sepal.Length,Sepal.Width,Petal.Length,Petal.Width)
-Vir <- data.frame(vir_b1)
-row.names(Vir) <- Vir$Species
-Vir <- subset(Vir, select = c(Sepal.Length,Sepal.Width,Petal.Length,Petal.Width))
-Vir
-
-barp <- data.frame(Setosa = Set, Versicolor=Ver, Virginica=Vir)
-
-barp
-barplot(as.matrix(barp), main = "num2 barplot", col ="green")
-
+barplot(as.matrix(Tr), main = "num2 barplot", beside =T,col=brewer.pal(n=4,name='RdBu'),
+        ylim = c(0, 10))
+legend(11, 10, c("Sepal.Length", "Sepal.Width", "Petal.Length","Petal.Width"),
+       cex = 0.8, fill = brewer.pal(n=nrow(Tr),name = 'RdBu'))
 #--(2) 가로---#
-t(barp)
-barplot(t(barp), main = "num2 barplot", col ="green", space = 0.1, cex.axis = 0.8,las=1)
+t(Tr)
+barplot(t(Tr), main = "num2 barplot", 
+        col=brewer.pal(n=length(t(Tr)),name="Set3"),ylim = c(0,20),  
+        space = 0.1, cex.axis = 1,las=0.2)
+legend(3, 18, c("Setosa", "Versicolor", "virginica"),
+       cex = 0.8, fill = brewer.pal(n=length(t(Tr)),name = 'Set3'))
 
+########################3번 입니다. ##################
 
-
+b1 <- boxplot(x1, col = brewer.pal(n=nrow(x1), name = "Pastel1"), main = "Setosa Sepal Box", names=c("Sepal.Length","Sepal width"))
+b2 <- boxplot(x2, col = brewer.pal(n=nrow(x2), name = "Pastel1"), main = "Setosa Petal Box", names=c("Petal.Length","Petal width"))
+b3 <- boxplot(y1, col = brewer.pal(n=nrow(y1), name = "Pastel1"), main = "versicolor Sepal Box", names=c("Sepal.Length","Sepal width"))
+b4 <- boxplot(y2, col = brewer.pal(n=nrow(y2), name = "Pastel1"), main = "versicolor Petal Box", names=c("Petal.Length","Petal width"))
+b5 <- boxplot(z1, col = brewer.pal(n=nrow(z1), name = "Pastel1"), main = "virginica Sepal Box", names=c("Sepal.Length","Sepal width"))
+b6 <- boxplot(z2, col = brewer.pal(n=nrow(z2), name = "Pastel1"), main = "virginica Petal Box", names=c("Petal.Length","Petal width"))
+par(mfrow=c(2,3))
