29ae78beef09485d38989f7814b7442393d5f4e6
noc1992 <noc817@gmail.com>
2019-06-13 00:46:17 +0900

    160612 Last

--- Part2/stage2/Ex_hiphop/Ex_hiphop_1.R
+++ Part2/stage2/Ex_hiphop/Ex_hiphop_1.R
@@ -4,7 +4,8 @@
 # 2. Top 10 단어에 대해서 원 그래프 만들기 (ggplot 사용할 것)
 # 3. Top 10 단어에 대해서 막대 그래프 만들기 (ggplot 사용할 것)
 # 4. ppt로 작성해서 제출할것 
-install.packages("openNLP")
+devtools::install_github('cran/ggplot2')
+install.packages("tidyverse")
 library(KoNLP)
 library(RColorBrewer)
 library(NLP)
@@ -17,6 +18,9 @@ library(dplyr)
 useSejongDic()
 library(wordcloud2)
 library(wordcloud)
+library(ggmosaic)
+library(tidyverse)
+
 
 hh <- readLines("hiphop.txt")
 hh1 <- gsub("'\n'","", hh)
@@ -41,17 +45,17 @@ for (i in 1:cnt_txt) {
 }
 
 
-write(unlist(hh2),"rehiphop.txt")
-data <- read.table("rehiphop.txt")
+write(unlist(hh2),"r_hh.txt")
+data <- read.table("r_hh.txt")
 
 nrow(data)
 wordcount1 <- table(data)
 result <- head(sort(wordcount1,decreasing = T),50)
-
-wordcloud(names(wordcount1),freq = wordcount1, scale=c(3,1),
-          rot.per=0.5,min.freq=15,random.order=F,random.color=T,
-          colors=brewer.pal(n=11,name = "Set2"))
-legend(0.07,0.92, "hiphop 노래 가사속의 단어 빈도수", cex = 0.8,fill=NA,border=NA,bg="white",
+result
+wordcloud(names(result),freq = result, scale=c(3,1),
+          rot.per=0.5,min.freq=3,random.order=F,random.color=T,
+          colors=brewer.pal(n=7,name = "Set2"))
+legend(0.25,0.92, "hiphop 노래 가사속의 단어 빈도수", cex = 0.8,fill=NA,border=NA,bg="white",
        text.col="black",text.font=3, box.col="white")
 wordcloud2(result,size = 0.5)
 
@@ -59,26 +63,15 @@ wordcloud2(result,size = 0.5)
 ###그래프###
 result1 <- head(sort(wordcount1,decreasing = T),10)
 df <- as.data.frame(result1)
-
+result1
 df
-
-rev <- read.table('rehiphop.txt')
-
-ggplot(df,aes(x=data,y=Freq)) + geom_bar(stat = 'identity', width = 0.8)
+write(names(result1), "rs.txt")
+red <- read.table("rs.txt")
+red
+ggplot(df,aes(x=data,y=Freq,fill=Freq)) + geom_bar(stat = 'identity', width = 0.8)
 
 #############
-
-df_top10 <- df %>%
-  mutate(pct = Freq /sum(Freq) * 100) %>%
-  mutate(ylabel = paste(sprintf("%s\n%4.1f",rev,pct), '%',sep = '')) %>%
-  arrange(desc(rev)) %>%
-  mutate(ypos = cumsum(pct) - 0.5*pct)
-
-df_top10
-ggplot(df, aes(x='',y=Freq,fill=rev)) + 
-  geom_bar(width = 1, stat = 'identity') + coord_polar("y", start = 0) +
-  geom_text(aes(y=ypos,label=ylabel),color='black')
-
-
-
-
+pct = pct = df$Freq /sum(df$Freq) * 100
+ylabel = paste(sprintf("%s\n%4.1f",red, pct), '%',sep = '')
+pct
+ggplot(df) + geom_mosaic(aes(weight=Freq, x='',fill=data)) + coord_polar(theta = "y",start = 0) 
